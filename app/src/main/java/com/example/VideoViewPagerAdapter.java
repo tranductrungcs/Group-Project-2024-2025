cda715520cc436377428f507784917fac671db44
Hoang-Doan123 <hoangdd.ba12-078@st.usth.edu.vn>
2024-12-27 21:16:23 +0700

    Change small things

--- app/src/main/java/com/example/VideoViewPagerAdapter.java
+++ app/src/main/java/com/example/VideoViewPagerAdapter.java
@@ -19,22 +19,26 @@ public class VideoViewPagerAdapter extends FragmentStatePagerAdapter {
             case 1: return new VideoSamsungFragment();
             case 2: return new VideoHuaweiFragment();
             case 3: return new VideoXiaomiFragment();
+            case 4: return new VideoMicrosoftFragment();
+            case 5: return new VideoAsusFragment();
         }
     }
 
     @Override
     public int getCount() {
-        return 4;
+        return 6;
     }
 
-//    @Nullable
-//    @Override
-//    public CharSequence getPageTitle(int position) {
-//        switch (position) {
-//            default: return "Apple";
-//            case 1: return "Samsung";
-//            case 2: return "Huawei";
-//            case 3: return "Xiaomi";
-//        }
-//    }
+    @Nullable
+    @Override
+    public CharSequence getPageTitle(int position) {
+        switch (position) {
+            default: return "Apple";
+            case 1: return "Samsung";
+            case 2: return "Huawei";
+            case 3: return "Xiaomi";
+            case 4: return "Microsoft";
+            case 5: return "Asus";
+        }
+    }
 }
