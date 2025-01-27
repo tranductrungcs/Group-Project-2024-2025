014a862e15ad40860b0beaa1a50ff12d30c767a3
lamdd.ba12-102@st.usth.edu.vn <you@example.com>
2025-01-27 17:08:22 +0700

    Update logo

--- app/src/main/java/com/example/MainActivity.java
+++ app/src/main/java/com/example/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
